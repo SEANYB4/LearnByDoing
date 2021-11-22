f2b6b8c8599379c97de5ae3d4fba399bc2e0bdb2
Sean Bain <75031999+SEANYB4@users.noreply.github.com>
2021-11-22 20:07:47 +0000

    Initial Commit

---
+++ LearnByDoing/LearnByDoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnByDoingApp.swift
+//  LearnByDoing
+//
+//  Created by Sean Bain on 22/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearnByDoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
